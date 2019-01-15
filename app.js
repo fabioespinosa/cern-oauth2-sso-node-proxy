98786c7857eb6649f482c0f7474dfe727d984d46
Fabio <fa.espinosa10@uniandes.edu.co>
2019-01-15 13:02:12 +0100

    Revert "add certs"
    This reverts commit 381216c5501960fefb3eb8ea8be5966bfad27e73.

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-const fs = require('fs');
 const express = require('express');
-const https = require('https');
-
 const app = express();
-
 const port = 3000;
 
-const privateKey = fs.readFileSync('sslcert/key.pem', 'utf8');
-const certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
-
-const server_options = {
-    key: privateKey,
-    cert: certificate
-};
-
 app.get('/', (req, res) => res.send('SSO Hello world'));
-const https_server = https.createServer(server_options, app);
-https_server.listen(port, () => console.log('SSO Hello world started'));
+app.listen(port, () => console.log('SSO Hello world started'));
