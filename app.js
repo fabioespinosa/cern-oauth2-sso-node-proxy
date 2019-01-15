381216c5501960fefb3eb8ea8be5966bfad27e73
Fabio <fa.espinosa10@uniandes.edu.co>
2019-01-15 12:33:47 +0100

    add certs

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
 const express = require('express');
+const https = require('https');
+
 const app = express();
+
 const port = 3000;
 
+const privateKey = fs.readFileSync('sslcert/key.pem', 'utf8');
+const certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+
+const server_options = {
+    key: privateKey,
+    cert: certificate
+};
+
 app.get('/', (req, res) => res.send('SSO Hello world'));
-app.listen(port, () => console.log('SSO Hello world started'));
+const https_server = https.createServer(server_options, app);
+https_server.listen(port, () => console.log('SSO Hello world started'));
