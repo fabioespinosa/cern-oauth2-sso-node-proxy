c9aa6b30968e1fdbdf007c73bb1cce63fd8981a5
Fabio <fa.espinosa10@uniandes.edu.co>
2019-01-17 15:03:38 +0100

    finish

--- app.js
+++ app.js
@@ -8,50 +8,45 @@ const port = 3000;
 
 const proxy = httpProxy.createProxyServer({});
 
-// passport.use(
-//     new OAuth2Strategy(
-//         {
-//             authorizationURL: 'https://oauth.web.cern.ch/OAuth/Authorize',
-//             tokenURL: 'https://oauth.web.cern.ch/OAuth/Token',
-//             clientID: 'cms-runregistry',
-//             clientSecret: 'h00FZSTyBjlalYKp7GqdBZQP2heMeaqLnpSNiTNXvcc1',
-//             callbackURL: 'https://cmsrunregistry.web.cern.ch/callback'
-//         },
-//         function(accessToken, refreshToken, profile, done) {
-//             profile.accessToken = accessToken;
-//             console.log('profile', profile);
-//             done(null, profile);
-//         }
-//     )
-// );
+app.use(passport.initialize());
+passport.use(
+    new OAuth2Strategy(
+        {
+            authorizationURL: 'https://oauth.web.cern.ch/OAuth/Authorize',
+            tokenURL: 'https://oauth.web.cern.ch/OAuth/Token',
+            clientID: 'cms-runregistry',
+            clientSecret: 'h00FZSTyBjlalYKp7GqdBZQP2heMeaqLnpSNiTNXvcc1',
+            callbackURL: 'https://cmsrunregistry.web.cern.ch/callback'
+        },
+        function(accessToken, refreshToken, profile, done) {
+            profile.accessToken = accessToken;
+            console.log('profile', profile);
+            done(null, profile);
+        }
+    )
+);
 
-app.get('*', (req, res) => {
-    // console.log('hola');
-    // res.send('it works');
-    proxy.web(req, res, {
-        target: 'http://cms-rr-prod.cern.ch:7001'
-    });
+app.get(
+    '/callback',
+    passport.authenticate('oauth2', {
+        failureRedirect: '/error',
+        session: false
+    }),
+    function(req, res) {
+        console.log(req.user.displayName);
+        console.log(req.headers);
+        res.redirect('/secret');
+    }
+);
+app.get('/error', (req, res) => {
+    res.send('Error authenticating user');
 });
 
-// app.use(passport.initialize());
-// app.get(
-//     '/callback',
-//     passport.authenticate('oauth2', {
-//         failureRedirect: '/error',
-//         session: false
-//     }),
-//     function(req, res) {
-//         console.log(req.user.displayName);
-//         console.log(req.headers);
-//         res.send('authenticated');
-//         // res.redirect('/proxy/online/runs/all');
-//     }
-// );
-// app.get('/error', (req, res) => {
-//     res.send('Error authenticating user');
-// });
+app.get('/secret', passport.authenticate('oauth2'), (req, res) => {
+    res.send('this is secret and everything worked');
+});
 
-// app.get('/proxy/*', passport.authenticate('oauth2'), (req, res) => {
+// app.get('*', passport.authenticate('oauth2'), (req, res) => {
 //     console.log('hola');
 //     res.send('it works');
 //     // proxy.web(req, res, {
