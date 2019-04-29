e28aae61f8049e5028e2ceac4a9097ab3ef67f46
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2019-04-29 18:34:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,21 +13,7 @@ const proxy = httpProxy.createProxyServer({});
 
 
 // This proxy redirects API requests and client side requests
-// API requests (GET, POST, PUT, ...):
-if (process.env.API_URL) {
-    app.all('/api/*'
-            //, isUserAuthenticated
-            ,  (req, res) => {
-        // Remove the API from path
-        const new_path = req.url.split('/api')[1];
-        req.path = new_path;
-        req.url = new_path;
-        req.originalUrl = new_path;
-        proxy.web(req, res, {
-            target: process.env.API_URL
-        });
-    });
-}
+
 
 
 app.use(cookieParser());
@@ -101,7 +87,21 @@ app.get('/error', (req, res) => {
 
 
 
-
+// API requests (GET, POST, PUT, ...):
+if (process.env.API_URL) {
+    app.all('/api/*'
+            , isUserAuthenticated
+            ,  (req, res) => {
+        // Remove the API from path
+        const new_path = req.url.split('/api')[1];
+        req.path = new_path;
+        req.url = new_path;
+        req.originalUrl = new_path;
+        proxy.web(req, res, {
+            target: process.env.API_URL
+        });
+    });
+}
 
 // Client requests
 app.all('*', isUserAuthenticated, (req, res) => {
