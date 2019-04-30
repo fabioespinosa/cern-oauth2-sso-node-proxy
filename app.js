34b67ba081f70f5bcd09fbb2f8fb13cca2b6aa5f
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2019-04-30 13:21:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ passport.deserializeUser(function(user, done) {
 // Middleware to check if the user is authenticated
 function isUserAuthenticated(req, res, next) {
     if (req.user) {
-        console.log('authed', req.originalUrl);
         next();
     }
     else {
