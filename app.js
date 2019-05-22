0284503d74158b8a474c30b4b692511b625d8ac3
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2019-05-22 15:27:23 +0200

    remove logs

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ passport.use(
     )
 );
 
-console.log(JSON.stringify(process.env));
 // Used to stuff a piece of information into a cookie
 passport.serializeUser(function(user, done) {
     done(null, user);
