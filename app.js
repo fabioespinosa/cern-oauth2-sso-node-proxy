67ef6affc09924e6ad88cab5eb12b143227bb095
Fabio <fa.espinosa10@uniandes.edu.co>
2020-04-10 00:58:20 +0200

    prettier and using http server

--- app.js
+++ app.js
@@ -14,13 +14,12 @@ const port = 8080;
 
 const long_timeout = 100000000000;
 const proxy = httpProxy.createProxyServer({
-    timeout: long_timeout,
-    proxyTimeout: long_timeout
+  timeout: long_timeout,
+  proxyTimeout: long_timeout,
 });
 
 app.use(cookieParser());
 app.use(session({ secret: 'cern' }));
-
 
 // USE OF BODY PARSER will make proxying of API unusuable (for POST and PUT): see https://github.com/chimurai/http-proxy-middleware/issues/299
 // app.use(bodyParser.json());
@@ -29,120 +28,117 @@ app.use(passport.initialize());
 app.use(passport.session()); // Used to persist login sessions
 
 passport.use(
-    new OAuth2Strategy(
-        {
-            authorizationURL:
-                process.env.authorizationURL ||
-                'https://oauth.web.cern.ch/OAuth/Authorize',
-            tokenURL:
-                process.env.tokenURL || 'https://oauth.web.cern.ch/OAuth/Token',
-            clientID: process.env.clientID,
-            clientSecret: process.env.clientSecret,
-            callbackURL: process.env.callbackURL
-        },
-        function(accessToken, refreshToken, profile, done) {
-            done(null, profile);
-        }
-    )
+  new OAuth2Strategy(
+    {
+      authorizationURL:
+        process.env.authorizationURL ||
+        'https://oauth.web.cern.ch/OAuth/Authorize',
+      tokenURL: process.env.tokenURL || 'https://oauth.web.cern.ch/OAuth/Token',
+      clientID: process.env.clientID,
+      clientSecret: process.env.clientSecret,
+      callbackURL: process.env.callbackURL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
 );
 
 // Used to stuff a piece of information into a cookie
-passport.serializeUser(function(user, done) {
-    done(null, user);
+passport.serializeUser(function (user, done) {
+  done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
+passport.deserializeUser(function (user, done) {
+  done(null, user);
 });
 
 // Middleware to check if the user is authenticated
 function isUserAuthenticated(req, res, next) {
-    // If the user is authenticated in django (using custom auth, let it through)
-    if (req.cookies.sessionid) {
-        next();
-    }
-    if (req.user) {
-        next();
-    }
-    else {
-        req.session.returnTo = req.originalUrl;
-        res.redirect('/callback');
-    }
+  // If the user is authenticated in django (using custom auth, let it through)
+  if (req.cookies.sessionid) {
+    next();
+  }
+  if (req.user) {
+    next();
+  } else {
+    req.session.returnTo = req.originalUrl;
+    res.redirect('/callback');
+  }
 }
 
 app.get(
-    '/callback',
-    passport.authenticate('oauth2', {
-        failureRedirect: '/error'
-    }),
-    function(req, res) {
-        res.redirect(req.session.returnTo || '/');
-        delete req.session.returnTo;
-    }
+  '/callback',
+  passport.authenticate('oauth2', {
+    failureRedirect: '/error',
+  }),
+  function (req, res) {
+    res.redirect(req.session.returnTo || '/');
+    delete req.session.returnTo;
+  }
 );
 
 // Logout route
 app.get('/logout', (req, res) => {
-    req.session.destroy(function() {
-        res.clearCookie('connect.sid');
-        res.redirect('https://login.cern.ch/adfs/ls/?wa=wsignout1.0');
-    });
+  req.session.destroy(function () {
+    res.clearCookie('connect.sid');
+    res.redirect('https://login.cern.ch/adfs/ls/?wa=wsignout1.0');
+  });
 });
 app.get('/error', (req, res) => {
-    res.send('Error authenticating user');
+  res.send('Error authenticating user');
 });
 
 // This proxy can also work with an API if provided the API_URL env. variable:
 // Authentication will work normally for a browser that accesses the API, since it already has the session cookie. For API use, it will need to provide with all the cookies after OAUTH
 // API requests (GET, POST, PUT, ...):
 if (process.env.API_URL) {
-    app.all('/api/*', isUserAuthenticated, (req, res) => {
-        // Remove the api from url:
-        const new_path = req.url.split('/api')[1];
-        req.path = new_path;
-        req.url = new_path;
-        req.originalUrl = new_path;
-        proxy.on('proxyReq', (proxyReq, req, res, options) => {
-            const { user } = req;
-            if (user) {
-                proxyReq.setHeader('displayname', user.displayname);
-                proxyReq.setHeader('egroups', user.egroups);
-                proxyReq.setHeader('email', user.email);
-                proxyReq.setHeader('id', user.id);
-            }
-        });
-        proxy.web(req, res, {
-            target: process.env.API_URL
-        });
+  app.all('/api/*', isUserAuthenticated, (req, res) => {
+    // Remove the api from url:
+    const new_path = req.url.split('/api')[1];
+    req.path = new_path;
+    req.url = new_path;
+    req.originalUrl = new_path;
+    proxy.on('proxyReq', (proxyReq, req, res, options) => {
+      const { user } = req;
+      if (user) {
+        proxyReq.setHeader('displayname', user.displayname);
+        proxyReq.setHeader('egroups', user.egroups);
+        proxyReq.setHeader('email', user.email);
+        proxyReq.setHeader('id', user.id);
+      }
     });
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+    });
+  });
 }
 
 // Client requests
 app.all('*', isUserAuthenticated, (req, res) => {
-    proxy.on('proxyReq', (proxyReq, req, res, options) => {
-        const { user } = req;
-        if (user) {
-            proxyReq.setHeader('displayname', user.displayname);
-            proxyReq.setHeader('egroups', user.egroups);
-            proxyReq.setHeader('email', user.email);
-            proxyReq.setHeader('id', user.id);
-        }
-    });
-    
-    proxy.web(req, res, {
-        target: process.env.CLIENT_URL
-    });
+  proxy.on('proxyReq', (proxyReq, req, res, options) => {
+    const { user } = req;
+    if (user) {
+      proxyReq.setHeader('displayname', user.displayname);
+      proxyReq.setHeader('egroups', user.egroups);
+      proxyReq.setHeader('email', user.email);
+      proxyReq.setHeader('id', user.id);
+    }
+  });
+
+  proxy.web(req, res, {
+    target: process.env.CLIENT_URL,
+  });
 });
-
-
 
 // If something goes wrong on either API or client:
-proxy.on('error', function(err, req, res) {
-    res.writeHead(500, {
-        'Content-Type': 'text/plain'
-    });
+proxy.on('error', function (err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain',
+  });
 
-    res.end(`${err.message} ----------- ${JSON.stringify(err)}`);
+  res.end(`${err.message} ----------- ${JSON.stringify(err)}`);
 });
 
-app.listen(port, () => console.log(`OAUTH Proxy started on port ${port}`));
+const server = http.createServer(app);
+server.listen(port, () => console.log(`OAUTH Proxy started on port ${port}`));
