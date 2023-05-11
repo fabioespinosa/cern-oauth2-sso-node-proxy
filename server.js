f7848e2f190555b19cd21d8223a4d0e6ce47aaa3
Dimitris Papagiannis <d.papag.a@gmail.com>
2023-05-11 16:59:35 +0200

    More tests with server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const session = require('express-session');
 const Keycloak = require('keycloak-connect');
-
+const http = require('http');
+const httpProxy = require('http-proxy');
+const PORT = 8080;
 
 let keycloak_config = {
   //   'issuer': 'https://auth.cern.ch/auth/realms/cern',
@@ -27,22 +29,37 @@ let keycloak_config = {
   //   bearerOnly: true,
   //   serverUrl: process.env.CLIENT_URL
   'realm': 'cern',
-  'realm-public-key':
-      'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrVrCuTtArbgaZzL1hvh0xtL5mc7o0NqPVnYXkLvgcwiC3BjLGw1tGEGoJaXDuSaRllobm53JBhjx33UNv+5z/UMG4kytBWxheNVKnL6GgqlNabMaFfPLPCF8kAgKnsi79NMo+n6KnSY8YeUmec/p2vjO2NjsSAVcWEQMVhJ31LwIDAQAB',
   'auth-server-url': 'https://auth.cern.ch/auth',
   'ssl-required': 'external',
   'resource': process.env.clientID,
-  'public-client': true,
+  'redirect-uri': process.env.callbackURL,
+  'credentials': {'secret': process.env.clientSecret},
+  'end_session_endpoint':
+      'https://auth.cern.ch/auth/realms/cern/protocol/openid-connect/logout',
+  'jwks_uri':
+      'https://auth.cern.ch/auth/realms/cern/protocol/openid-connect/certs',
 };
 
 
-
+const app = express();
 const memoryStore = new session.MemoryStore({checkPeriod: 86400000});
-
 const keycloak = new Keycloak({store: memoryStore}, keycloak_config);
 
+const long_timeout = 2147483640;
+const proxy = httpProxy.createProxyServer({
+  timeout: long_timeout,
+  proxyTimeout: long_timeout,
+});
 
-const app = express();
+const server = http.createServer(app);
+server.setTimeout(500000);
+server.timeout = 100 * 60 * 1000;
+
+server.on('upgrade', function(req, res, head) {
+  proxy.ws(req, res, head, {target: process.env.API_URL});
+});
+
+
 // Configure session
 app.use(session({
   secret: 'cern',
@@ -52,11 +69,26 @@ app.use(session({
 }));
 
 // app.set('trust proxy', true);
-app.use(keycloak.middleware());
+app.use(keycloak.middleware({logout: '/logout'}));
 
-app.listen(8080, function() {
-  console.log('App listening on port 3000');
+// app.get('/logout', (req, res, next) => {
+//   req.session.destroy(function() {
+//     console.log('!!logging outttt');
+//     // res.clearCookie('connect.sid');
+//     res.redirect('');
+//   });
+// })
+// app.get('/callback', , function(req, res, next) {
+//   res.redirect(req.session.returnTo || '/');
+//   delete req.session.returnTo;
+// });
+
+
+app.all('*', keycloak.protect(), (req, res, next) => {
+  console.log(Object.keys(req));
+  res.json({'message': true})
 });
 
-app.get(
-    '/', keycloak.protect(), (req, res, next) => {res.json({status: 'ok'})});
+app.listen(PORT, function() {
+  console.log(`App listening on port ${PORT}`);
+});
